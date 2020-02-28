1ccf005ccd78d90f4370eef753e034d474fbf8ab
Terranie Sims <terranie67@gmail.com>
2020-02-28 14:13:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(players, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = ["He was the last beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var newFacts = [];
   var i = 0;
   while (newFacts.length < facts.length) {
