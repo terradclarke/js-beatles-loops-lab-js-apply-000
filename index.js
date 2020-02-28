cbb1d2b1e1385733599c7a10cbd88649d22a8c76
Terranie Sims <terranie67@gmail.com>
2020-02-28 12:51:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 // add solution here
 
-var musicians = ["John", "Paul", "George", "Ringo"];
-
-var instruments = ["guitar", "guitar", "bass", "drums"];
-
-function theBeatlesPlay (musicians, instruments) {
-  var array = [];
-  for (let i = 0; i < 4; i++)
-  {
-    array.push('${musicians[0]} plays ${instruments[0]}');
-  }
-  return array;
-}
 
 function johnLennonFacts() {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
