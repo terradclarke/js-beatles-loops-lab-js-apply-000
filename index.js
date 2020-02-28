9e2f59f8cc39f47efcdb440734b301fd65870a6c
Terranie Sims <terranie67@gmail.com>
2020-02-28 13:07:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(players, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice", "While on tour, he enjoyed playing Monopoly", "Harmonica was the first instrument Lennon learned to play"];
   var newFacts = [];
   var i = 0;
   while (i < facts.length) {
