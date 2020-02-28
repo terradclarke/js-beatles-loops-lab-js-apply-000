e1230b122139f530a92ef93ea441214540290c7a
Terranie Sims <terranie67@gmail.com>
2020-02-28 13:05:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function johnLennonFacts() {
   var newFacts = [];
   var i = 0;
   while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!");
+    newFacts.push(facts[i] 
+    + "!!!");
     i++;
   }
   return newFacts;
