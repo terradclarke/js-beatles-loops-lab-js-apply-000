9383e0b285066005d68c5e980c246d5bccf524e4
Terranie Sims <terranie67@gmail.com>
2020-02-28 13:02:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // add solution here
-
+function theBeatlesPlay(players, instruments) {
+  var allPlayers = [];
+  for (var i=0; i < players.length; i++) {
+    allPlayers.push(players[i] 
+    + " plays " 
+    + instruments[i]);
+  }
+  return allPlayers
+}
 
 function johnLennonFacts() {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
